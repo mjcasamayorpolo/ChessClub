211c0070b89bcf4c394aea2f8badd1ec2e5f11a0
Maria Jose Casamayor Polo <mcasamayorpolo@uoc.edu>
2023-05-06 19:37:15 +0200

    Fullcalendar dependency

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,4 +1,6 @@
 import * as bootstrap from 'bootstrap';
+import { Tooltip } from "bootstrap";
+import { Calendar } from 'fullcalendar';
 import lazyframe from "lazyframe";
 
 var myCarousel = document.querySelector('#carouselExampleCaptions')
@@ -18,3 +20,58 @@ lazyframe(lazyVideo,{
     autoplay: false
 });
 videoContainer.appendChild(lazyVideo);
+
+const calendarEl = document.getElementById('calendar')
+const calendar = new Calendar(calendarEl, {
+  initialView: 'dayGridMonth',
+  locale: 'es-ES',
+  headerToolbar: { start: 'title', center: '', end: 'prev,next' },
+  firstDay: 1,
+  events: [
+    {
+      id: 'mayo01',
+      title: 'Torneo',
+      description: 'Torneo de ajedrez del Mediterráneo',
+      start: '2023-05-20'
+    },
+    {
+      id: 'mayo02',
+      title: 'Clase',
+      description: 'Clase de ajedrez - Horario: 18:00-19:00',
+      start: '2023-05-02'
+    },
+    {
+      id: 'mayo03',
+      title: 'Clase',
+      description: 'Clase de ajedrez - Horario: 18:00-19:00',
+      start: '2023-05-09'
+    },
+    {
+      id: 'mayo04',
+      title: 'Clase',
+      description: 'Clase de ajedrez - Horario: 18:00-19:00',
+      start: '2023-05-16'
+    },
+    {
+      id: 'mayo05',
+      title: 'Clase',
+      description: 'Clase de ajedrez - Horario: 18:00-19:00',
+      start: '2023-05-23'
+    },
+    {
+      id: 'mayo06',
+      title: 'Clase',
+      description: 'Clase de ajedrez - Horario: 18:00-19:00',
+      start: '2023-05-30'
+    }
+  ],
+  eventDidMount: function(info) {
+    var tooltip = new Tooltip(info.el, {
+      title: info.event.extendedProps.description,
+      placement: 'top',
+      trigger: 'hover',
+      container: 'body'
+    });
+  }
+});
+calendar.render();
